0bb1a8f9140e10b1c7c3c1ba35058072d3b36144
CB-19 <46390830+CB-19@users.noreply.github.com>
2020-08-02 17:56:49 -0400

    Add files via upload

--- student-app/course_load.cpp
+++ student-app/course_load.cpp
@@ -39,7 +39,7 @@ int main(void)
 		cout << "Conection isValid: " << con_ptr->isValid() << endl;
 
 		std::string course_id, sec_num, student_id, pswd;
-		int num_courses, course_limit = 5;
+		int num_courses, max_enroll, enrolled_students, course_limit = 5;
 		char reg;
 
 		cout << "Please enter your Student ID: ";
@@ -71,49 +71,54 @@ int main(void)
 				cin >> course_id;
 				cout << "Please enter a Section Number: ";
 				cin >> sec_num;
-				//query to check if the student is already in the course they are trying to register for
-				std::unique_ptr<sql::Statement> stmt2(con_ptr->createStatement());
+				//query to check if the course and section actually exists
 				std::unique_ptr<sql::ResultSet>
-				res2 (stmt2->executeQuery("SELECT S_ID FROM REGISTRATION WHERE S_ID = '" + student_id + "' AND Course_ID = '" + course_id + "'"));
-				if(res2->next())
-					cout << "You are already registered for this course. Please choose another." << endl;
+				c_s_check (stmt-> executeQuery("SELECT Course_ID, Sec_No FROM SECTION WHERE Course_ID = '" + course_id + "' and Sec_No = '" + sec_num + "'"));
+				if (c_s_check -> next()){
+					cout << "This course exists" << endl;
+					//query to check if the student is already in the course they are trying to register for
+					std::unique_ptr<sql::Statement> stmt2(con_ptr->createStatement());
+					std::unique_ptr<sql::ResultSet>
+					res2 (stmt2->executeQuery("SELECT S_ID FROM REGISTRATION WHERE S_ID = '" + student_id + "' AND Course_ID = '" + course_id + "'"));
+					if(res2->next())
+						cout << "You are already registered for this course. Please choose another." << endl;
 				
-				else{
-					cout << "Registering you for Course: " << course_id << ", Section: " << sec_num << endl;
-					std::unique_ptr<sql::Statement> stmt3(con_ptr->createStatement());
-                                        stmt3->executeUpdate("INSERT INTO REGISTRATION VALUES ('" + student_id + "', '" + course_id + "', '" + sec_num + "')");
-					num_courses += 1;
+					else{
+						//checks if class is full
+						cout << "Checking if class is full" << endl;
+						std::unique_ptr<sql::Statement> stmt3(con_ptr->createStatement());
+		                                std::unique_ptr<sql::ResultSet>
+		                                full (stmt3->executeQuery("SELECT r.course_id, r.sec_no, count(s_id) AS NumStud, Max_Enroll FROM REGISTRATION r, SECTION s WHERE r.course_Id = '" + course_id + "' AND r.sec_no = '" + sec_num + "' AND r.Course_ID = s.course_id AND r.sec_no = s.sec_no GROUP BY r.course_id, s.sec_no"));
+
+					while(full -> next()){
+					        enrolled_students = full-> getInt("NumStud");
+		                		max_enroll = full-> getInt("Max_Enroll");
+					}
+						
+						int seats_left = max_enroll - enrolled_students;
+			                        if (seats_left > 0){
+				                       	cout << "You can enroll for this course!";
+			        	               	cout << "There are " << seats_left << " seats left!" << endl;
+							cout << "Registering you for Course: " << course_id << ", Section: " << sec_num << endl;
+				        		std::unique_ptr<sql::Statement> stmt4(con_ptr->createStatement());
+							stmt4->executeUpdate("INSERT INTO REGISTRATION VALUES ('" + student_id + "', '" + course_id + "', '" + sec_num + "')");
+							num_courses += 1;
+							cout << "You have registered for " << num_courses << " classes." << endl;
+						}	
+						else
+							cout << "Sorry, this class is full. Please select another course." << endl;
+					}			
 				}
-
-				if(num_courses == 5)
-					cout << "You have reached the limit of classes you can register for." << endl;
+				else
+					cout << "This course does not exists" << endl;
 			}
-			else
+			else{
+				cout << "Thank you for registering." << endl;
 				break;
-		}
-
-
-		/*
-		while(res->next()){
-			cout << "Course ID = " << res -> getString("Course_Id");
-			cout << ", Section Number = " << res -> getString("Sec_No");
-			enrolled_students = res-> getInt("NumStud");
-			cout << ", Number of Students = " << enrolled_students;
-			max_enroll = res-> getInt("Max_Enroll");
-			cout << ", Maximum number of students = " << max_enroll << endl;
-			
-			int seats_left = max_enroll - enrolled_students;
-			if (seats_left > 0){
-				cout << "You can enroll for this course!";
-				cout << "There are " << seats_left << " seats left!" << endl;
-				//insert into table
 			}
-			else
-				cout << "Sorry this course is full." <<endl;
 		}
-		*/
-			
-
+	if (num_courses == course_limit)
+		cout << "You are already registered for all of your courses. Thank you have a nice day." << endl;	
 
 	} catch (sql::SQLException &e) {
 		/*
